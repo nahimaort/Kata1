5ea69d7706b1a5661de65a98bdaaeba7108833c8
nahimaortega <nahimaortega@localhost>
2021-09-23 13:07:56 +0100

    Primera versión de Kata1

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,11 @@
+package kata1;
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person person = new Person("", new Date(99, 1, 1));
+        System.out.println(person.getName() +  " tiene " + person.getAge());
+    }
+    
+}
