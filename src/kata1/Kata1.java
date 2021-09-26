f4d4a5649127a7170e17fc9ee22b39d9ee54cba5
nahimaortega <nahimaortega@localhost>
2021-09-26 20:21:19 +0100

    Primera versión de Kata1

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -4,7 +4,7 @@ import java.util.Date;
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("", new Date(99, 1, 1));
+        Person person = new Person("Juan", new Date(99, 1, 1));
         System.out.println(person.getName() +  " tiene " + person.getAge());
     }
     
